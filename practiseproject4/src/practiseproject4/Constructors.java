87eae8d5a7c64af28b26663aee64fc9d72a31c2f
vinaysaimaineni <vinaysaimaineni05@gmail.com>
2022-07-24 00:08:37 +0530

    changes

--- practiseproject4/src/practiseproject4/Constructors.java
+++ practiseproject4/src/practiseproject4/Constructors.java
@@ -3,3 +3,23 @@ package practiseproject4;
 public class Constructors {
 
 }
+//default constructor
+class EmpInfo{
+	int id;
+	String name;
+
+void display() {
+	System.out.println(id+" "+name);
+	}
+
+
+public static void main(String[] args) {
+
+	EmpInfo emp1=new EmpInfo();
+	EmpInfo emp2=new EmpInfo();
+
+	emp1.display();
+	emp2.display();
+	}
+}
+
