301c23a44d45585f2dc35e581e2fae1d8564f8f1
vinaysaimaineni <vinaysaimaineni05@gmail.com>
2022-07-25 16:44:01 +0530

    changes

---
+++ practiseproject9/src/practiseproject9/Arrays.java
@@ -0,0 +1,23 @@
+package practiseproject9;
+
+public class Arrays {
+
+
+
+public static void main(String[] args) {
+
+
+int a[]= {1,2,3,4,5,6,7,8,9};
+for(int i=0; i<7; i++) {
+System.out.println("Elements of array a: "+a[i]);
+}
+
+
+
+int[][] b = {
+            {2, 4, 6, 8, 5}, 
+            {3, 6, 9} };
+      
+      System.out.println("\nLength of row: " + b[0].length);
+      }
+}
